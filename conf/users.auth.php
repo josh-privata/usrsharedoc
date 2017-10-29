a19fe9f8bd2ee9a562c05c3bfbf6b3b11ab1841b
Josh Cannons <josh.cannons@gmail.com>
2017-10-29 20:16:52 +1000

    updates

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+josh:$1$4SGmnOqn$pqBF17HVExydJ97G.dOhs0:Josh Cannons:josh.cannons@gmail.com:admin,user
